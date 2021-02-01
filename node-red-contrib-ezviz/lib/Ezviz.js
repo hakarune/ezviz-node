94663999c0352977b70bfbde481f49b455b507ee
pkircher29 <52182240+pkircher29@users.noreply.github.com>
2021-02-01 02:57:19 -0500

    Update Ezviz.js

--- node-red-contrib-ezviz/lib/Ezviz.js
+++ node-red-contrib-ezviz/lib/Ezviz.js
@@ -4,10 +4,10 @@ const moment = require('moment')
 const array = require('lodash/array')
 const EzvizCode = require('./EzvizCode')
 
-// 有效期7天，提前8小时更新
-const EXPIRE_TIME =  (1000 * 3600 * 8 + 10 * 1000)
-// 打开方式
-const openType = { 0: '指纹', 1: '密码', 2: '卡' }
+// valid for 7 days, updated 8 hours in advance
+const EXPIRE_TIME = (1000 * 3600 * 8 + 10 * 1000)
+// open method
+const openType = {0:'fingerprint', 1:'password', 2:'card'}
 
 class EzvizClass {
   constructor (node, config) {
@@ -15,36 +15,36 @@ class EzvizClass {
     this.config = config
   }
   getToken () {
-    const { set: setCache, get: getCache } = this.node.context().global
-    const { client_id, client_secret } = this.config
+    const {set: setCache, get: getCache} = this.node.context().global
+    const {client_id, client_secret} = this.config
     const cache_key = `ys7-${client_id}`
     return new Promise(async (resolve, reject) => {
       try {
         const cache = getCache(cache_key)
-        if (cache && cache.time && ((cache.time - new Date().valueOf() ) > EXPIRE_TIME)) {
+        if (cache && cache.time && ((cache.time-new Date().valueOf())> EXPIRE_TIME)) {
           // console.log('has cache', cache)
           resolve(cache.token)
           return
         }
-        const { data } = await axios.post(`https://open.ys7.com/api/lapp/token/get?appKey=${client_id}&appSecret=${client_secret}`, {
-          params: { grant_type: 'client_credentials' },
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        const {data} = await axios.post(`https://open.ys7.com/api/lapp/token/get?appKey=${client_id}&appSecret=${client_secret}`, {
+          params: {grant_type:'client_credentials' },
+          headers: {'Content-Type':'application/x-www-form-urlencoded'}
         }).catch(err => {
-          throw new Error(`[萤石Token]${err}`)
+          throw new Error(`[Fluorite Token]${err}`)
         })
         if (data.code != 200) {
           const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石Token]${msg}`)
+          throw new Error(`[Fluorite Token]${msg}`)
         }
         if (!data.data.accessToken) {
-          reject(new Error('[萤石Token]token获取失败'))
+          reject(new Error('[Fluorite Token] token acquisition failed'))
         }
         setCache(cache_key, {
           token: data.data.accessToken,
           time: data.data.expireTime
         })
         resolve(data.data.accessToken)
-      } catch (err) { reject(err) }
+      } catch (err) {reject(err)}
     })
   }
 
@@ -53,15 +53,15 @@ class EzvizClass {
       try {
         const token = await this.getToken()
 
-        const { data } = await axios.post(`https://open.ys7.com/api/lapp/keylock/open/list?accessToken=${token}&deviceSerial=${config.payload}&pageStart=0&pageSize=20`, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        const {data} = await axios.post(`https://open.ys7.com/api/lapp/keylock/open/list?accessToken=${token}&deviceSerial=${config.payload}&pageStart=0&pageSize=20 `, {
+          headers: {'Content-Type':'application/x-www-form-urlencoded'}
         }).catch(err => {
-          throw new Error(`[萤石keylock]${err}`)
+          throw new Error(`[fluorite keylock]${err}`)
         })
 
         if (data.code != 200) {
           const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石keylock]${msg}`)
+          throw new Error(`[fluorite keylock]${msg}`)
         }
         let res = data.data
         if (res) {
@@ -71,14 +71,14 @@ class EzvizClass {
             return item
           })
         } else {
-          throw new Error(`[萤石keylock]无数据`)
+          throw new Error(`[Fluorite keylock] no data`)
         }
-        const { set: setCache, get: getCache } = this.node.context().global
-        const { client_id } = this.config
+        const {set: setCache, get: getCache} = this.node.context().global
+        const {client_id} = this.config
         const cache_key = `ys7-openData-${client_id}`
         const cache = getCache(cache_key) || []
-        // 对比和上次差异的数据
-        const diff = array.differenceBy(res, cache, 'openTime')
+        // Compare and last difference data
+        const diff = array.differenceBy(res, cache,'openTime')
 
         setCache(cache_key, res)
 
@@ -94,15 +94,15 @@ class EzvizClass {
       try {
         const token = await this.getToken()
 
-        const { data } = await axios.post(`https://open.ys7.com/api/lapp/device/capture?accessToken=${token}&deviceSerial=${config.payload}&channelNo=${config.channelNo}`, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        const {data} = await axios.post(`https://open.ys7.com/api/lapp/device/capture?accessToken=${token}&deviceSerial=${config.payload}&channelNo=${config.channelNo }`, {
+          headers: {'Content-Type':'application/x-www-form-urlencoded'}
         }).catch(err => {
-          throw new Error(`[萤石capture]${err}`)
+          throw new Error(`[fluorite capture]${err}`)
         })
 
         if (data.code != 200) {
           const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石capture]${msg}`)
+          throw new Error(`[fluorite capture]${msg}`)
         }
         resolve(data.data)
       } catch (error) {
@@ -110,21 +110,21 @@ class EzvizClass {
       }
     })
   }
-  //云台控制（调用预置点）
+  // PTZ control (call preset point)
   presetMove (config) {
     return new Promise(async (resolve, reject) => {
       try {
         const token = await this.getToken()
 
-        const { data } = await axios.post(`https://open.ys7.com/api/lapp/device/preset/move?accessToken=${token}&deviceSerial=${config.payload}&channelNo=${config.channelNo}&index=${config.index}`, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        const {data} = await axios.post(`https://open.ys7.com/api/lapp/device/preset/move?accessToken=${token}&deviceSerial=${config.payload}&channelNo=${config .channelNo}&index=${config.index}`, {
+          headers: {'Content-Type':'application/x-www-form-urlencoded'}
         }).catch(err => {
-          throw new Error(`[萤石capture]${err}`)
+          throw new Error(`[fluorite capture]${err}`)
         })
 
         if (data.code != 200) {
           const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石capture]${msg}`)
+          throw new Error(`[fluorite capture]${msg}`)
         }
         resolve(data.data)
       } catch (error) {
@@ -133,7 +133,7 @@ class EzvizClass {
     })
   }
 
-  //获取摄像头列表
+  //Get the camera list
   cameraList (config) {
     return new Promise(async (resolve, reject) => {
       try {
@@ -145,71 +145,5 @@ class EzvizClass {
         if(config.pageSize != null){
             url += "&pageSize="+config.pageSize
         }
-        const { data } = await axios.post(url, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        }).catch(err => {
-          throw new Error(`[萤石capture]${err}`)
-        })
-
-        if (data.code != 200) {
-          const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石capture]${msg}`)
-        }
-        resolve(data.data)
-      } catch (error) {
-        reject(error)
-      }
-    })
-  }
-
-  //获取镜头遮蔽开关状态
-  sceneSwitchStatus (config) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const token = await this.getToken()
-
-        const { data } = await axios.post(`https://open.ys7.com/api/lapp/device/scene/switch/status?accessToken=${token}&deviceSerial=${config.payload}`, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        }).catch(err => {
-          throw new Error(`[萤石capture]${err}`)
-        })
-
-        if (data.code != 200) {
-          const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石capture]${msg}`)
-        }
-        resolve(data.data)
-      } catch (error) {
-        reject(error)
-      }
-    })
-  }
-
-  //设置镜头遮蔽开关
-  sceneSwitchSet (config) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const token = await this.getToken()
-        var url = `https://open.ys7.com/api/lapp/device/scene/switch/set?accessToken=${token}&deviceSerial=${config.payload}&enable=${config.enable}`
-        if(config.channelNo != null ){
-          url = url += "&channelNo="+config.channelNo
-        }
-        const { data } = await axios.post(url, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        }).catch(err => {
-          throw new Error(`[萤石capture]${err}`)
-        })
-
-        if (data.code != 200) {
-          const msg = EzvizCode[data.code] || data.msg
-          throw new Error(`[萤石capture]${msg}`)
-        }
-        resolve(data.data)
-      } catch (error) {
-        reject(error)
-      }
-    })
-  }
-
-}
-module.exports = EzvizClass
+        const {data} = await axios.post(url, {
+          headers: {'Content-Type': '
