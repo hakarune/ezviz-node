b2f5a97fcfb32766a38706eed93830bc54e0ed80
Ace Elliott <hakarune@gmail.com>
2022-06-10 23:09:08 -0700

    Delete node-red-contrib-gps directory

--- node-red-contrib-gps/configurator.js
+++
@@ -1,8 +0,0 @@
-module.exports = (RED) => {
-  RED.nodes.registerType('gpsoo-configurator', class {
-    constructor (config) {
-      RED.nodes.createNode(this, config)
-      Object.assign(this, config)
-    }
-  })
-}
